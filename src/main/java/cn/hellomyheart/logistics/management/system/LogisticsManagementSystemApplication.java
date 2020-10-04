03650c793553d705e820f883daa70695136bbbbb
stephen shen <723809534@qq.com>
2020-10-04 09:19:46 +0800

    Initial commit

---
+++ src/main/java/cn/hellomyheart/logistics/management/system/LogisticsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package cn.hellomyheart.logistics.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/hellomyheart/logistics/management/system/LogisticsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hellomyheart.logistics.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
