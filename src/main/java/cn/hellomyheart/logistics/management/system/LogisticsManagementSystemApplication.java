e3f20b70d52d5b0772ceb48e1cc647b3b6ace77a
stephen shen <723809534@qq.com>
2020-10-04 10:56:00 +0800

    加入mapper包扫描注解

--- src/main/java/cn/hellomyheart/logistics/management/system/LogisticsManagementSystemApplication.java
+++ src/main/java/cn/hellomyheart/logistics/management/system/LogisticsManagementSystemApplication.java
@@ -1,9 +1,11 @@
 package cn.hellomyheart.logistics.management.system;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.hellomyheart.logistics.management.system.mapper")
 public class LogisticsManagementSystemApplication {
 
     public static void main(String[] args) {
