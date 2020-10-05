8ee8d3c5ceaba7bcc60b5b075c69d9845cbf2451
stephen shen <723809534@qq.com>
2020-10-05 10:11:15 +0800

    解决跨域问题

--- src/main/java/cn/hellomyheart/logistics/management/system/LogisticsManagementSystemApplication.java
+++ src/main/java/cn/hellomyheart/logistics/management/system/LogisticsManagementSystemApplication.java
@@ -3,9 +3,12 @@ package cn.hellomyheart.logistics.management.system;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @MapperScan("cn.hellomyheart.logistics.management.system.mapper")
+//使得@WebServlet、@WebFilter、@WebListener注解自动注册
+@ServletComponentScan
 public class LogisticsManagementSystemApplication {
 
     public static void main(String[] args) {
